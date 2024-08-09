d134639067c6ae8c0f521980fbdbd9cbdd981537
Nigel Peris <nperis27@gmail.com>
2024-08-09 12:00:00 +0530

    feat: add single type models

---
+++ strapi-backend/src/api/parish-council-member/controllers/parish-council-member.ts
@@ -0,0 +1,7 @@
+/**
+ * parish-council-member controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::parish-council-member.parish-council-member');
