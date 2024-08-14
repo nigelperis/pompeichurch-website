0759f042420cea4f9f1f5798033609e38a06ebc9
Nigel Peris <nperis27@gmail.com>
2024-08-14 12:00:00 +0530

    feat: add initial sections

--- astro-frontend/tailwind.config.mjs
+++ astro-frontend/tailwind.config.mjs
@@ -1,11 +1,53 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [
-		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
-		'./node_modules/flowbite/**/*.js',
-	],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'inknut-antiqua': ['Inknut Antiqua', defaultTheme.fontFamily.serif],
+				roboto: ['Roboto', defaultTheme.fontFamily.sans],
+			},
+			keyframes: {
+				slidein: {
+					from: {
+						opacity: '0',
+						transform: 'translateY(20px)',
+					},
+					to: {
+						opacity: '1',
+						transform: 'translateY(0)',
+					},
+				},
+				slideInLeft: {
+					from: {
+						opacity: '0',
+						transform: 'translateX(-100%)',
+					},
+					to: {
+						opacity: '1',
+						transform: 'translateX(0)',
+					},
+				},
+				slideInRight: {
+					from: {
+						opacity: '0',
+						transform: 'translateX(100%)',
+					},
+					to: {
+						opacity: '1',
+						transform: 'translateX(0)',
+					},
+				},
+			},
+		},
+		animation: {
+			slidein: 'slidein 1s ease-out var(--slidein-delay, 0) forwards',
+			'slidein-left': 'slideInLeft 1s ease-out forwards',
+			'slidein-right': 'slideInRight 1s ease-out forwards',
+		},
 	},
-	plugins: [require('flowbite/plugin')],
+
+	plugins: [],
 };
