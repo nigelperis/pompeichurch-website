ae692aaada85ec14c3d4b0206e2f8467ea41d010
Nigel Peris <nperis27@gmail.com>
2024-10-04 22:38:57 +0530

    refactor: modularize pastoral commissions

---
+++ astro-frontend/src/constants/commissions.ts
@@ -0,0 +1,106 @@
+const commissions = [
+	{
+		title: 'Liturgy-Word-Proclamation',
+		subtitles: [
+			{
+				commission: 'Commission for Bible Apostolate',
+				name: 'Mr. Henry Sequeira',
+			},
+			{
+				commission: 'Commission for Catechetics Apostolate',
+				name: 'Mrs. Jacintha Peris',
+			},
+			{
+				commission: 'Commission for Proclamation Apostolate',
+				name: 'Mrs. Gretta Lobo',
+			},
+			{ commission: 'Commission for Liturgy', name: 'Mrs. Praveena DSilva' },
+		],
+		convenor: 'Mrs. Linet Monteiro',
+	},
+	{
+		title: 'Witness',
+		subtitles: [
+			{
+				commission: 'Commission for Social Development Apostolate',
+				name: 'Mrs. Vijaya DCosta',
+			},
+			{
+				commission: 'Commission for Social Communication',
+				name: 'Mrs. Laveena Andrade',
+			},
+			{
+				commission: 'Commission for Justice and Peace Apostolate',
+				name: 'Michael Johnson',
+			},
+			{
+				commission: 'Commission for Formation of the Priests and the Religious',
+				name: 'Sr. Jaya',
+			},
+			{
+				commission: 'Commission for Environment Apostolate',
+				name: 'Mr. Charles Sequeira',
+			},
+		],
+		convenor: 'Roshan DChuna',
+	},
+	{
+		title: 'Service',
+		subtitles: [
+			{
+				commission: 'Commission for Education Apostolate',
+				name: 'Mrs. Gretta Nazareth',
+			},
+			{
+				commission: 'Commission for Health Apostolate',
+				name: 'Mrs. Wilma Crasta',
+			},
+			{
+				commission: 'Commission for Labour Apostolate',
+				name: 'Mrs. Laveena Rosario',
+			},
+			{
+				commission: "Commission for Migrants' Aposolate",
+				name: 'Sr. Leena Pereira',
+			},
+			{
+				commission: 'Commission for Apostolate to Specially Abled',
+				name: 'Mr. Melwin Saldanha',
+			},
+		],
+		convenor: 'Sr. Sadhana',
+	},
+	{
+		title: 'Unity',
+		subtitles: [
+			{
+				commission: 'Commission for Small Christian Community',
+				name: 'Mrs. Delcy Rodrigues',
+			},
+			{
+				commission: 'Commission for Family Apostolate',
+				name: 'Mr. Ivan DSouza',
+			},
+			{
+				commission: 'Commission for Laity Apostolate',
+				name: 'Mr. Alwyn Pinto',
+			},
+			{ commission: 'Commission for Women', name: 'Mrs. Juliet Serrao' },
+			{
+				commission: 'Commission for Youth Apostolate',
+				name: 'Mr. Godwin Tauro',
+			},
+			{
+				commission: 'Commission for Ecumenism Apostolate',
+				name: 'Mrs. Margarette Andrade',
+			},
+			{
+				commission: 'Commission for Inter-Religious Dialogue Apostolate',
+				name: 'Mr. Rajesh Noronha',
+			},
+		],
+		convenor: 'Mrs. Violet Peris',
+	},
+];
+
+export { commissions };
