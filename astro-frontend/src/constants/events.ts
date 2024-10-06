ad1055686a5e0ce0d5d960acb43edbac114ffea6
Nigel Peris <nperis27@gmail.com>
2024-10-06 08:52:39 +0530

    refactor: events

---
+++ astro-frontend/src/constants/events.ts
@@ -0,0 +1,21 @@
+import eventImage1 from '~/assets/centenary-hall.jpg';
+import eventImage from '~/assets/mass-interior.jpg';
+
+const events = [
+	{
+		eventName: 'Community Gathering',
+		imageUrl: eventImage,
+		description:
+			'Join us for a community gathering filled with fun, food, and fellowship.',
+		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+	},
+	{
+		eventName: 'Charity Drive',
+		imageUrl: eventImage1,
+		description:
+			'Help us support those in need by participating in our charity drive.',
+		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+	},
+];
+
+export { events };
