9a875a9b588043cf6912340bc531c59893aca131
Nigel Peris <nperis27@gmail.com>
2024-10-06 08:22:56 +0530

    refactor: obituary

---
+++ astro-frontend/src/constants/obituary-list.ts
@@ -0,0 +1,30 @@
+import dummyImage from '~/assets/dummy-image.png';
+
+const obituaryData = [
+	{
+		name: 'John Doe (69 Years)',
+		imageUrl: dummyImage,
+		description: 'xyz',
+		ward: 'St. Joseph',
+	},
+	{
+		name: 'Jane Smith (69 Years)',
+		imageUrl: dummyImage,
+		description: 'xyz',
+		ward: 'St. Mary',
+	},
+	{
+		name: 'Jane Smith (69 Years)',
+		imageUrl: dummyImage,
+		description: 'xyz',
+		ward: 'St. Mary',
+	},
+	{
+		name: 'Jane Smith (69 Years)',
+		imageUrl: dummyImage,
+		description: 'xyz',
+		ward: 'St. Mary',
+	},
+];
+
+export { obituaryData };
