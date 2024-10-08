8ff936eff446115e41bb610c4b7a79024ed25835
Nigel Peris <nperis27@gmail.com>
2024-10-08 17:04:53 +0530

    feat: add strapi utility functions

--- astro-frontend/astro.config.mjs
+++ astro-frontend/astro.config.mjs
@@ -1,3 +1,4 @@
+import node from '@astrojs/node';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
@@ -6,22 +7,37 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    prefetch: true,
-    integrations: [
-        tailwind(),
-        icon({
-            iconDir: 'src/assets/icons',
-        }),
-        react(),
-    ],
-    i18n: {
-        locales: ['en', 'kok'],
-        defaultLocale: 'en',
-        routing: {
-            redirectToDefaultLocale: true,
-        },
-    },
-    devToolbar: {
-        enabled: false,
-    },
+	prefetch: true,
+	output: 'server',
+	adapter: node({
+		mode: 'standalone',
+	}),
+	integrations: [
+		tailwind(),
+		icon({
+			iconDir: 'src/assets/icons',
+		}),
+		react(),
+	],
+	i18n: {
+		locales: ['en', 'kok'],
+		defaultLocale: 'en',
+		routing: {
+			redirectToDefaultLocale: true,
+		},
+	},
+
+	image: {
+		domains: ['astro.build', 'http://localhost:1337'],
+		// remotePatterns: [{ protocol: 'https' }, { protocol: 'http' }],
+	},
+
+	devToolbar: {
+		enabled: true,
+	},
+	vite: {
+		ssr: {
+			noExternal: ['aos'],
+		},
+	},
 });
