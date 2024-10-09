e6d2419bd8dc7ebbc601f404edd811558371ac21
Nigel Peris <nperis27@gmail.com>
2024-10-09 22:29:07 +0530

    fix: ui issue and format files

--- astro-frontend/src/constants/church-history.ts
+++ astro-frontend/src/constants/church-history.ts
@@ -1,7 +1,3 @@
 const churchHistory = `
-	The Gurpur Parish is situated at a distance of 19 K.M.
-						from Mangalore on Mangalore-Moodbidri road. The parish is locally known
-						as Kowdur. The Church land structure is located at a village called ‘Kandavar’.
-						Apart from Kowdur, Mulur, Badaga, Ulipadi, Paduperar, Mooduperar, Kolambe,
-						Gurpur, Addur villages are covered under the area of Gurpur Parish....`;
+	The Gurpur Parish is situated at a distance of 19 K.M. from Mangalore on Mangalore-Moodbidri road. The parish is locally known as Kowdur. The Church land structure is located at a village called ‘Kandavar’. Apart from Kowdur, Mulur, Badaga, Ulipadi, Paduperar, Mooduperar, Kolambe, Gurpur, Addur villages are covered under the area of Gurpur Parish. To describe the parish geographically, the area of the parish is covered with a number of fields and hills. To the south of the parish flows the Gurpur River...`;
 export { churchHistory };
