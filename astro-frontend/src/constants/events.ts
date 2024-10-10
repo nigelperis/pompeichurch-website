b6512c130eb2ded7382897345b1a70cbab22fccb
Nigel <nigel.peris@codecraft.co.in>
2024-10-10 20:47:08 +0530

    refactor: events page

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -6,14 +6,14 @@ const events = [
 		eventName: 'Community Gathering',
 		imageUrl: eventImage,
 		description:
-			'Join us for a community gathering filled with fun, food, and fellowship.',
+			'Join us for a community gathering filled with fun, food, and fellowship. Help us support those in need by participating in our charity drive.',
 		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
 	},
 	{
 		eventName: 'Charity Drive',
 		imageUrl: eventImage1,
 		description:
-			'Help us support those in need by participating in our charity drive.',
+			'Help us support those in need by participating in our charity drive. Help us support those in need by participating in our charity drive.',
 		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
 	},
 ];
