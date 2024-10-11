ff1c09ff6da87fdfebdb9d78d8259cdc86f227db
Nigel Peris <nperis27@gmail.com>
2024-10-11 23:07:04 +0530

    fix: responsive events

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -16,6 +16,20 @@ const events = [
 			'Help us support those in need by participating in our charity drive. Help us support those in need by participating in our charity drive.',
 		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
 	},
+	{
+		eventName: 'Community Gathering',
+		imageUrl: eventImage,
+		description:
+			'Join us for a community gathering filled with fun, food, and fellowship. Help us support those in need by participating in our charity drive.',
+		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+	},
+	{
+		eventName: 'Charity Drive',
+		imageUrl: eventImage1,
+		description:
+			'Help us support those in need by participating in our charity drive. Help us support those in need by participating in our charity drive.',
+		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+	},
 ];
 
 export { events };
