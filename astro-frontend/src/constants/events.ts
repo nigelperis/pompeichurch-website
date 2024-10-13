3d516e50762b3de6950a4a2f5316c489344d9d04
Nigel Peris <nperis27@gmail.com>
2024-10-13 16:33:20 +0530

    refactor: parish history

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -1,19 +1,20 @@
 import event2 from '~/assets/event2.jpg'
 import event1 from '~/assets/event1.jpg';
+
 const events = [
 	{
 		eventName: 'A Day with the Lord',
 		imageUrl: event2,
 		description:
 			'A day of adoration, gratitude, forgiveness, and heartfelt intentions in the presence of the Lord',
-		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+		facebookLink: 'https://facebook.com/event-link', // TODO (Aroma): replace links
 	},
 	{
 		eventName: 'PYC League 2024',
 		imageUrl: event1,
 		description:
 			'Enthusiatic Football and Cricket League hosted by ICYM - PYC for Gurpur Parishoners  ',
-		facebookLink: 'https://facebook.com/event-link', // Replace with actual Facebook event link
+		facebookLink: 'https://facebook.com/event-link', // TODO (Aroma): replace links
 	},
 ];
 
