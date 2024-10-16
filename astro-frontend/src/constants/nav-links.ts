e312dc8150c86f728536afeaf37256cbfe1a9b38
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2024-10-16 23:17:58 +0530

    refactor : nav-bar

--- astro-frontend/src/constants/nav-links.ts
+++ astro-frontend/src/constants/nav-links.ts
@@ -17,6 +17,11 @@ export type NavItem = Expandable | Link;
 
 export const navLinks = [
 	{
+		type: 'link',
+		title: 'nav.home',
+		href: '/',
+	},
+	{
 		type: 'expandable',
 		title: 'nav.our-church',
 		expandedLinks: [
