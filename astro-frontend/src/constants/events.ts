dfa43b770637bbb8d1d6b2389c4eeff06c5333ce
Nigel Peris <nperis27@gmail.com>
2024-10-16 12:00:00 +0530

    refactor: slug approach for events and clean hoverable component

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -1,20 +1,23 @@
 import event1 from '~/assets/event1.jpg';
 import event2 from '~/assets/event2.jpg';
 
+// TODO (Aroma): update facebook links
 const events = [
 	{
 		eventName: 'A Day with the Lord',
+		slug:"a-day-with-the-lord",
 		imageUrl: event2,
 		description:
 			'A day of adoration, gratitude, forgiveness, and heartfelt intentions in the presence of the Lord',
-		facebookLink: 'https://facebook.com/event-link', // TODO (Aroma): replace links
+		facebookLink: 'https://facebook.com/event-link',
 	},
 	{
 		eventName: 'PYC League 2024',
+		slug:"pyc-league-2024",
 		imageUrl: event1,
 		description:
-			'Enthusiatic Football and Cricket League hosted by ICYM - PYC for Gurpur Parishoners  ',
-		facebookLink: 'https://facebook.com/event-link', // TODO (Aroma): replace links
+			'Enthusiatic Football and Cricket League hosted by ICYM - PYC for Gurpur Parishoners',
+		facebookLink: 'https://facebook.com/event-link',
 	},
 ];
 
