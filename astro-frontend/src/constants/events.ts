520c8acc46d0d93186dd1fb43fa1c45a32d6bc67
Nigel Peris <nperis27@gmail.com>
2024-10-20 22:01:40 +0530

    fix: build issues and format code

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -5,7 +5,7 @@ import event2 from '~/assets/event2.jpg';
 const events = [
 	{
 		eventName: 'A Day with the Lord',
-		slug:"a-day-with-the-lord",
+		slug: 'a-day-with-the-lord',
 		imageUrl: event2,
 		description:
 			'A day of adoration, gratitude, forgiveness, and heartfelt intentions in the presence of the Lord',
@@ -13,7 +13,7 @@ const events = [
 	},
 	{
 		eventName: 'PYC League 2024',
-		slug:"pyc-league-2024",
+		slug: 'pyc-league-2024',
 		imageUrl: event1,
 		description:
 			'Enthusiatic Football and Cricket League hosted by ICYM - PYC for Gurpur Parishoners',
