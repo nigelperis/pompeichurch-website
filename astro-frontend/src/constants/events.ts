6bada0684f1043050d847012ccb74a563fa5fa92
Aroma Peris <paroma3008@gmail.com>
2024-10-22 23:37:42 +0530

    feat: add more events

--- astro-frontend/src/constants/events.ts
+++ astro-frontend/src/constants/events.ts
@@ -1,15 +1,52 @@
 import event1 from '~/assets/event1.jpg';
 import event2 from '~/assets/event2.jpg';
+import event3 from '~/assets/event3.jpg';
+import event4 from '~/assets/event4.jpg';
+import event5 from '~/assets/event5.jpg';
+import event6 from '~/assets/event6.jpg';
+
 
 // TODO (Aroma): update facebook links
 const events = [
+	{
+		eventName: 'Life of a Saint Enactment Competition',
+		slug: 'life-of-a-saint-enactment-competition',
+		imageUrl: event6,
+		description:
+			'A creative competition organized by ICYM-PYC, wards enacting the inspiring lives of various saints through performances.',
+		facebookLink: 'https://www.facebook.com/share/p/4SFQGJhW8ff5YFvL/',
+	},
+	{
+		eventName: 'Mission Sunday - 2024',
+		slug: 'mission-sunday-2024',
+		imageUrl: event5,
+		description:
+			'Celebrate and support the Church`s mission work through prayers, reflections, donations, and community engagement activities.',
+		facebookLink: 'https://www.facebook.com/share/p/7VE3q9Bh8rCEXdVZ/',
+	},
+	{
+		eventName: 'Blessing of Vehicles ',
+		slug: 'blessing-of-vehicles',
+		imageUrl: event4,
+		description:
+			'A blessing ceremony where vehicles were blessed for safety and protection in the community.',
+		facebookLink: 'https://www.facebook.com/share/p/NPTp5ADmUMFJaMdj/',
+	},
+	{
+		eventName: 'Welcome Ceremony for Deacon Ashwin Periera',
+		slug: 'welcome-ceremony-for-deacon-ashwin-periera',
+		imageUrl: event3,
+		description:
+			'A heartfelt ceremony welcoming Deacon Ashwin Pereira to our church community with prayers and blessings.',
+		facebookLink: 'https://www.facebook.com/share/p/c36mshHfh4CXpyem/',
+	},
 	{
 		eventName: 'A Day with the Lord',
 		slug: 'a-day-with-the-lord',
 		imageUrl: event2,
 		description:
 			'A day of adoration, gratitude, forgiveness, and heartfelt intentions in the presence of the Lord',
-		facebookLink: 'https://facebook.com/event-link',
+		facebookLink: 'https://www.facebook.com/share/p/EtmT92AHGXZtgRzF/',
 	},
 	{
 		eventName: 'PYC League 2024',
@@ -17,7 +54,7 @@ const events = [
 		imageUrl: event1,
 		description:
 			'Enthusiatic Football and Cricket League hosted by ICYM - PYC for Gurpur Parishoners',
-		facebookLink: 'https://facebook.com/event-link',
+		facebookLink: 'https://www.facebook.com/share/p/dH6rYXy5bWXUMCpW/',
 	},
 ];
 
