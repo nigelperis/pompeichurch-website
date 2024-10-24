4ffe8a5b4c72ecb07a540e402260ef2feacc53c9
Aroma Peris <paroma3008@gmail.com>
2024-10-25 00:14:14 +0530

    feat: develop prayer corner

---
+++ astro-frontend/src/constants/prayer1.ts
@@ -0,0 +1,7 @@
+const prayer = `
+     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates vero
+    numquam dolore, molestias ipsa voluptate consequatur ratione illo quas
+    voluptatem blanditiis ex dolorem doloremque at obcaecati reprehenderit
+    voluptatum aliquid fugiat, nisi fuga.`
+    ;
+export {prayer};
