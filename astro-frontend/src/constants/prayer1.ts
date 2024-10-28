0cf0f1b656e3eaee8cc57438c1682a6351de5c68
Aroma Peris <paroma3008@gmail.com>
2024-10-29 00:10:19 +0530

    feat: add daily prayer cards

--- astro-frontend/src/constants/prayer1.ts
+++ astro-frontend/src/constants/prayer1.ts
@@ -1,7 +1,116 @@
-const prayer = `
-     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates vero
-    numquam dolore, molestias ipsa voluptate consequatur ratione illo quas
-    voluptatem blanditiis ex dolorem doloremque at obcaecati reprehenderit
-    voluptatum aliquid fugiat, nisi fuga.`
-    ;
-export {prayer};
+const prayers =[
+    {
+        ourFather: `Our Father, who art in heaven, hallowed be
+        thy name. Thy kingdom come, thy will be done,
+        on earth, as it is in heaven. Give us this day our
+        daily bread and forgive us our trespasses as we
+        forgive those who trespass against us; and lead us
+        not into temptation, but deliver us from evil.`
+    },
+    {
+        hailMary: `
+            Hail Mary,
+            Full of Grace,
+            The Lord is with thee.
+            Blessed art thou among women,
+            and blessed is the fruit
+            of thy womb, Jesus.
+            Holy Mary,
+            Mother of God,
+            pray for us sinners now,
+            and at the hour of our death.`
+    },
+    {
+        gloryBe: `
+            Glory be to the Father,
+            and to the Son,
+            and to the Holy Spirit.
+            As it was in the beginning, is now,
+            and ever shall be,
+            world without end.`
+    },
+    {
+        morningOffering: `O Jesus, through the Immaculate Heart of Mary,
+            I offer You my prayers, works, joys, and sufferings
+            of this day in union with the Holy Sacrifice of the Mass throughout the world.
+            I offer them for all the intentions of Your Sacred Heart:
+            the salvation of souls, reparation for sin, and the reunion of all Christians.
+            I offer them for the intentions of our bishops and of all Apostles of Prayer,
+            and in particular for those recommended by our Holy Father this month. Amen`
+    },
+    {
+        consecrationToMary:`My Queen and my Mother, I give myself entirely to you;
+            and to show my devotion to you, I
+            consecrate to you this day my eyes, my ears, my
+            mouth, my heart, my whole being without reserve.
+            Wherefore, good Mother, as I am your own, keep me, guard me, as your property and
+            possession. Amen.`
+    },
+    {
+        actOfFaith:`O my God, I firmly believe that Thou art one God,
+            in three Divine Persons, the Father, the Son and the Holy Ghost;
+            I believe that Thy Divine Son became man and
+            died for our sins and that He will come to judge the living and the dead.
+            I believe these and all the truths which the holy Catholic Church teaches,
+            because Thou hast revealed them, Who canst neither deceive nor be deceived.`
+    },
+    {
+        actOfHope:`O My God, relying on Thy infinite goodness and promises,
+            I hope to obtain pardon of my sins, the help of Thy grace and life everlasting,
+            through the merits of Jesus Christ, my Lord and Redeemer.`
+    },
+    {
+        actOfLove:`O my God, I love Thee above all things with my whole heart and soul,
+            because Thou art all good and worthy of all love.
+            I love my neighbor as myself for love of Thee.
+            I forgive all who have injured me, and I ask pardon for all whom I have injured.`
+    },
+    {
+        actOfContrition:`My God, I am sorry for my sins with all my heart.
+            In choosing to do wrong and failing to do good,
+            I have sinned against you whom I should love above all things.
+            I firmly intend, with your help, to do penance, to sin no more,
+            and to avoid whatever leads me to sin.
+            Our Savior Jesus Christ suffered and died for us.
+            In His name. My God have mercy.`
+    },
+    {
+        guardianAngelPrayer:`Angel of God,
+            my guardian dear,
+            To whom God's love
+            commits me here,
+            Ever this day,
+            be at my side,
+            To light and guard,
+            Rule and guide.`
+    },
+    {
+        eternalRestPrayer:`Eternal rest grant unto them,
+            O Lord, and let perpetual light shine upon them.
+            May they rest in peace. Amen.`
+    },
+    {
+        theAngelus:`
+        <strong>The Angel of the Lord declared to Mary:</strong>
+        <br>And she conceived of the Holy Spirit.
+
+        <br><i>Hail Mary . . .
+
+        <br><strong>Behold the handmaid of the Lord:</strong>
+        <br><i>Be it done unto me according to Thy word.
+
+        <br>Hail Mary . . .
+
+        <br><strong>And the Word was made Flesh:</strong>
+        <br>And dwelt among us.
+
+        <br><i>Hail Mary . . .</i>
+
+        <br><strong>Pray for us, O Holy Mother of God, that we may be made worthy of the promises of Christ.</strong>
+
+        <br><strong><center>Let us pray:</strong>
+
+        <br>Pour forth, we beseech Thee, O Lord, Thy grace into our hearts; that we, to whom the incarnation of Christ, Thy Son, was made known by the message of an angel, may by His Passion and Cross be brought to the glory of His Resurrection, through the same Christ Our Lord.`
+    },
+];
+export {prayers};
