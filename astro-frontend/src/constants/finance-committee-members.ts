0aa4cbb43707de6748c2eab4d82c6a400cb06c3a
Nigel Peris <nperis27@gmail.com>
2024-10-31 12:44:30 +0530

    feat: update images

--- astro-frontend/src/constants/finance-committee-members.ts
+++ astro-frontend/src/constants/finance-committee-members.ts
@@ -2,7 +2,6 @@ import priestImage from '~/assets/fr-rudolf-dsa.jpg';
 import jamesMenda from '~/assets/mr-james-menda.jpg';
 import praveenLobo from '~/assets/mr-praveen-lobo.jpg';
 import vijayaDcosta from '~/assets/mrs-vijaya-dcosta.jpg';
->>>>>>> 40918ddfc735bd929c3b8d5e7c12811804518211
 import vicepresident from '~/assets/vp-image.jpg';
 import johnLobo from '~/assets/mr-john-lobo.jpg'
 
