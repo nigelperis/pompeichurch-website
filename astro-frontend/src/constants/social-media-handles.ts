fa578f6a06ed0d8ebb878ccef7c9a98e41a86daa
Aroma Peris <paroma3008@gmail.com>
2024-11-16 16:20:10 +0530

    feat: add media handles of PYC & YCS

---
+++ astro-frontend/src/constants/social-media-handles.ts
@@ -0,0 +1,17 @@
+const handles = [
+  {
+    name: 'icym-pyc',
+    links: [
+      { key: 'instagram1', value: 'https://www.instagram.com/pompei_youngsters_club/profilecard/?igsh=a3JwZWN6ZnJ1c3I4' },
+      { key: 'blogger', value: 'https://pompeiyoungstersclub.blogspot.com/?m=1' },
+    ],
+  },
+  {
+    name: 'ycs',
+    links: [
+      { key: 'instagram1', value: 'https://www.instagram.com' },
+    ],
+  },
+];
+
+export { handles };
