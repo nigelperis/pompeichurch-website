11741e48b861b285d0add8b3476a48e570753d78
Aroma Peris <paroma3008@gmail.com>
2024-11-17 22:05:12 +0530

    fix: change links

--- astro-frontend/src/constants/social-media-handles.ts
+++ astro-frontend/src/constants/social-media-handles.ts
@@ -2,14 +2,14 @@ const socials = [
   {
     name: 'icym-pyc',
     links: [
-      { key: 'instagram1', value: 'https://www.instagram.com/pompei_youngsters_club/profilecard/?igsh=a3JwZWN6ZnJ1c3I4' },
+      { key: 'instagram1', value: 'https://www.instagram.com/pompei_youngsters_club/' },
       { key: 'blogger', value: 'https://pompeiyoungstersclub.blogspot.com/?m=1' },
     ],
   },
   {
     name: 'ycs',
     links: [
-      { key: 'instagram1', value: 'https://www.instagram.com/ycs_gurpur/profilecard/?igsh=ZG5vNmhwZDExMzMx' },
+      { key: 'instagram1', value: 'https://www.instagram.com/ycs_gurpur/' },
     ],
   },
 ];
