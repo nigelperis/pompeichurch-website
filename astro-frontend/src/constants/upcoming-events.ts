14d5fbf1fa0d9917c345aaddc0f439633e28227f
Nigel Peris <nperis27@gmail.com>
2024-11-18 20:57:30 +0530

    fix: padding in socials

--- astro-frontend/src/constants/upcoming-events.ts
+++ astro-frontend/src/constants/upcoming-events.ts
@@ -1,6 +1,6 @@
 import cartoonia from '~/assets/cartoonia.jpg';
-import pcl2 from '~/assets/pcl-poster-2.jpg';
 import pcl1 from '~/assets/pcl-poster-1.jpg';
+import pcl2 from '~/assets/pcl-poster-2.jpg';
 
 const upcomingEvents = [
 	{
@@ -8,11 +8,11 @@ const upcomingEvents = [
 		class: 'w-full rounded-sm object-cover md:w-1/4',
 	},
 	{
-		eventImg: pcl2,
+		eventImg: pcl1,
 		class: 'w-full rounded-sm object-cover md:w-1/3',
 	},
 	{
-		eventImg: pcl1,
+		eventImg: pcl2,
 		class: 'w-full rounded-sm object-cover md:w-1/3',
 	},
 ];
