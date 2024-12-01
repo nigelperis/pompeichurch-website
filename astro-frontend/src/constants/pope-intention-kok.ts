76154200b4b029cab6bfb5047931dbe270a5ea7e
Aroma Peris <paroma3008@gmail.com>
2024-12-01 15:22:37 +0530

    refactor: add month field

--- astro-frontend/src/constants/pope-intention-kok.ts
+++ astro-frontend/src/constants/pope-intention-kok.ts
@@ -1,9 +1,65 @@
 const popeIntentionKok = [
 	{
+		month: 1,
+		title: '',
+		description: '',
+	},
+	{
+		month: 2,
+		title: '',
+		description: '',
+	},
+	{
+		month: 3,
+		title: '',
+		description: '',
+	},
+	{
+		month: 4,
+		title: '',
+		description: '',
+	},
+	{
+		month: 5,
+		title: '',
+		description: '',
+	},
+	{
+		month: 6,
+		title: '',
+		description: '',
+	},
+	{
+		month: 7,
+		title: '',
+		description: '',
+	},
+	{
+		month: 8,
+		title: '',
+		description: '',
+	},
+	{
+		month: 9,
+		title: '',
+		description: '',
+	},
+	{
+		month: 10,
+		title: '',
+		description: '',
+	},
+	{
+		month: 11,
 		title: `ನವೆಂಬರ್ ಮ್ಹಯ್ಯಾಕ್ ಪಾಪಾಚೊ ಇರಾದೊ`,
 		description:
 			'<strong>ಭುರ್ಗೆಂ ಹೊಗ್ಡಾಯ್ಲ್ ಲ್ಯಾ ಕೊಣಾಯ್ ಪಾಸತ್:</strong><br>ಪೂತ್ ವಾ ಧುವೆಕ್ ಹೊಗ್ಡಾಯ್ಲ್ ಲ್ಯಾ ದುಖಾಂತ್ ಆಸಾತ್ ತ್ಯಾ ಜಲ್ಮ್ ದಾತಾರಾಂಕ್ ಆಪ್ಲಾ ಸಮುದಾಯಾಂನಿ ಆಧಾರ್ ಮೆಳುಂ ಆನಿ ಪವಿತ್ರ್ ಆತ್ಮ್ಯಾ  ಥಾವ್ನ್ ಸಮಾಧಾನ್ ಆನಿ ಬುಜವಣ್ ಲಾಬುಂ ಮ್ಹಣ್ ಮಾಗ್ತಾಂವ್.',
 	},
+	{
+		month:12,
+		title: 'ದಸೆಂಬರ್ ಮ್ಹಯ್ಯಾಕ್ ಪಾಪಾಚೊ ಇರಾದೊ',
+		description: '<strong>ಭರ್ವಶಾಚ್ಯಾ ಯಾತ್ರಿಕಾಂ ಪಾಸತ್:</strong><br>ಹೆಂ ಜುಬ್ಲೆವ್ ವರಸ್ ಆಮ್ಚೊ ಭಾವಾರ್ಥ್ ಧೃಡ್ ಕರುಂ, ಆಮ್ಚ್ಯಾ ದಿಸ್ಪಡ್ತ್ಯಾ  ಜಿವಿತಾಂತ್ ಪುನರ್‌ಜಿವಂತ್ ಜೆಜುಕ್ ವಳ‍್ಕುಂಕ್ ಆಧಾರ್ ದಿಂವ್, ಆನಿ ಹೆಂ ಆಮ್ಕಾಂ ಕ್ರಿಸ್ತಿ ಭರ್ವಶಾಚಿಂ ಯಾತ್ರಿಕಾಂ ಜಾವ್ನ್ ಬದ್ಲುಂ ಮ್ಹಣ್ ಮಾಗ್ತಾಂವ್.',
+	}
 ];
 
 export { popeIntentionKok };
--- astro-frontend/src/constants/pope-intention.ts
+++ astro-frontend/src/constants/pope-intention.ts
@@ -1,9 +1,65 @@
 const popeIntention = [
 	{
+		month: 1,
+		title: '',
+		description: '',
+	},
+	{
+		month: 2,
+		title: '',
+		description: '',
+	},
+	{
+		month: 3,
+		title: '',
+		description: '',
+	},
+	{
+		month: 4,
+		title: '',
+		description: '',
+	},
+	{
+		month: 5,
+		title: '',
+		description: '',
+	},
+	{
+		month: 6,
+		title: '',
+		description: '',
+	},
+	{
+		month: 7,
+		title: '',
+		description: '',
+	},
+	{
+		month: 8,
+		title: '',
+		description: '',
+	},
+	{
+		month: 9,
+		title: '',
+		description: '',
+	},
+	{
+		month: 10,
+		title: '',
+		description: '',
+	},
+	{
+		month: 11,
 		title: `Pope's Intention for the month of November`,
 		description:
 			'<strong>For anyone who has lost a child:</strong><br>We pray that all parents who mourn the loss of a son or daughter find support in their community and receive peace and consolation from the Holy Spirit.',
 	},
+	{
+		month:12,
+		title: 'For pilgrims of hope',
+		description: 'We pray that this Jubilee Year strengthen our faith, helping us to recognize the Risen Christ in our daily lives, and that it may transform us into pilgrims of Christian hope.',
+	}
 ];
 
 export { popeIntention };
