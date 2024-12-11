9d0bacc2d6a7f62ccc7084fd12db5edc99d69fba
Aroma Peris <paroma3008@gmail.com>
2024-12-11 12:52:57 +0530

    feat: stay tuned image
    Add: New scroll bar design and nav minor bug fixes
    
    Feat: Smooth lenis Scrolling
    
    fix : nav sublink top scroll disabled
    
    Remove : smooth lenis scrolling
    
    fix: lang check in src
    
    fix: rename to filteredEvents

--- astro-frontend/src/constants/upcoming-events.ts
+++ astro-frontend/src/constants/upcoming-events.ts
@@ -1,17 +1,26 @@
+import pcl from '~/assets/pcl.jpg';
 import cartoonia from '~/assets/cartoonia.jpg';
 import pcl1 from '~/assets/pcl-poster-1.jpg';
 import pcl2 from '~/assets/pcl-poster-2.jpg';
 
 const upcomingEvents = [
 	{
+		endDate: '2024-12-15',
+		eventImg: pcl,
+		class: 'w-full rounded-sm object-cover md:w-1/3',
+	},
+	{
+		endDate: '2024-11-27',
 		eventImg: cartoonia,
 		class: 'w-full rounded-sm object-cover md:w-1/4',
 	},
 	{
+		endDate: '2024-12-15',
 		eventImg: pcl1,
 		class: 'w-full rounded-sm object-cover md:w-1/3',
 	},
 	{
+		endDate: '2024-12-15',
 		eventImg: pcl2,
 		class: 'w-full rounded-sm object-cover md:w-1/3',
 	},
