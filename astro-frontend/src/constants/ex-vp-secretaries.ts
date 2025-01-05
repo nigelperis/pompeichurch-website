358b36131eeb25c394baa7998aca99c9b62282cd
Nigel <nperis27@gmail.com>
2025-01-05 22:46:09 +0530

    feat: vp-secs done and dusted

--- astro-frontend/src/constants/ex-vp-secretaries.ts
+++ astro-frontend/src/constants/ex-vp-secretaries.ts
@@ -1,127 +1,130 @@
 //Vice-Presidents
-import georgePinto from '~/assets/vice-presidents/george-pinto.jpeg';
 import eliasRodrigues from '~/assets/vice-presidents/elias-rodrigues.jpeg';
+import georgePinto from '~/assets/vice-presidents/george-pinto.jpeg';
 import jeraldRasquinha from '~/assets/vice-presidents/jerald-rasquinha.jpeg';
-import richardLobo from '~/assets/vice-presidents/richard-lobo.jpeg';
-import wilfredPinto from '~/assets/vice-presidents/wilfred-pinto.jpeg';
-import johnsonLobo from '~/assets/vice-presidents/johnson-lobo.jpeg';
-import romansLobo from '~/assets/vice-presidents/romans-lobo.jpeg';
+import johnsonLobo from '~/assets/vice-presidents/johnson-lobo.jpg';
 import richardFernandes from '~/assets/vice-presidents/richard-fernandes.jpeg';
+import richardLobo from '~/assets/vice-presidents/richard-lobo.jpeg';
+import romansLobo from '~/assets/vice-presidents/romans-lobo.jpg';
+import wilfredPinto from '~/assets/vice-presidents/wilfred-pinto.jpeg';
 
 //Secretaries
-import alexDsouza from '~/assets/secretaries/alex-dsouza.jpeg';
 import agnesRodrigues from '~/assets/secretaries/agnes-rodrigues.jpeg';
-import srJacintha from '~/assets/secretaries/sr-jacintha.jpeg';
+import alexDsouza from '~/assets/secretaries/alex-dsouza.jpeg';
 import benedictPeris from '~/assets/secretaries/benedict-peris.jpeg';
-import oswaldPeris from '~/assets/secretaries/oswald-peris.jpeg';
-import lillyMathias from '~/assets/secretaries/lilly-mathias.jpeg';
 import jeraldLobo from '~/assets/secretaries/jerald-lobo.jpeg';
+import lillyMathias from '~/assets/secretaries/lilly-mathias.jpeg';
+import oswaldPeris from '~/assets/secretaries/oswald-peris.jpeg';
+import srJacintha from '~/assets/secretaries/sr-jacintha.jpeg';
 
-const vpSecretaries = [
-  //Vice-Presidents
+const vicePresidents = [
 	{
 		imageUrl: georgePinto,
 		name: 'georgepinto',
-		term1: '06-03-1986 : 31-05-1991',
+		term
+    : '06-03-1986 : 31-05-1991',
 	},
-  {
+	{
 		imageUrl: eliasRodrigues,
-		name: 'eliassrodrigues',
-		term1: '01-06-1991 : 31-05-1994',
+		name: 'eliasrodrigues',
+		term: '01-06-1991 : 31-05-1994',
 	},
-  {
+	{
 		imageUrl: jeraldRasquinha,
-		name: 'jerardlobo',
+		name: 'jeraldrasquinha',
 		term1: '01-06-1994 : 31-05-1998',
 	},
-  {
+	{
 		imageUrl: richardLobo,
 		name: 'richardlobo',
-		term1: '01-06-1998 : 31-05-2000',
+		term: '01-06-1998 : 31-05-2000',
 	},
-  {
-    imageUrl: wilfredPinto,
-    name: 'wilfredpinto',
-    term1: '01-06-2000 : 31-05-2003',
-  },
-  {
-    imageUrl: johnsonLobo,
-    name: 'johnsonlobo',
-    term1: '01-06-2003 : 31-05-2010',
-    term2: '01-01-2014 : 31-12-2016',
-  },
-  {
-    imageUrl: romansLobo,
-    name: 'romanslobo',
-    term1: '01-01-2011 : 31-12-2013',
-    // term2: ''
-  },
-  {
-    imageUrl: richardFernandes,
-    name: 'richardfernandes',
-    term1: '01-01-2017 : 31-12-2019',
-    term2: '01-01-2020 : 31-12-2020',
-  },
-  //Secretaries
-  {
-		imageUrl: alexDsouza,
-		name: 'alexdsouza',
-		term1: '01-02-1982 : 31-05-1985',
+	{
+		imageUrl: wilfredPinto,
+		name: 'wilfredpinto',
+		term: '01-06-2000 : 31-05-2003',
 	},
-  {
-		imageUrl: agnesRodrigues,
-		name: 'agnesrodrigues',
-		term1: '01-06-1985 : 30-11-1985',
+	{
+		imageUrl: johnsonLobo,
+		name: 'johnsonlobo',
+		term1: '01-06-2003 : 31-05-2010',
+		term2: '01-01-2014 : 31-12-2016',
 	},
-  {
-		imageUrl: srJacintha,
-		name: 'srjacintha',
-		term1: '01-12-1985 : 31-05-1986',
+	{
+		imageUrl: romansLobo,
+		name: 'romanslobo',
+		term: '01-01-2011 : 31-12-2013',
+		// term2: ''
 	},
-  {
-    imageUrl: benedictPeris,
-    name: 'benedictperis',
-    term1: '01-06-1986 : 31-05-1988',
-  },
-  {
-    imageUrl: oswaldPeris,
-    name: 'oswaldperis',
-    term1: '01-06-1988 : 31-05-1991',
-    term2:'01-06-2006 : 31-12-2010',
-  },
-  {
-    imageUrl: jeraldRasquinha,
-    name: 'jeraldrasquinha',
-    term1: '01-06-1991 : 31-05-1994',
-  },
-  {
-    imageUrl: wilfredPinto,
-    name: 'wilfredpinto',
-    term1: '01-06-1994 : 31-05-2000',
-    term2: '01-06-2006 : 31-12-2010',
-  },
-  {
-    imageUrl: lillyMathias,
-    name: 'lillymathias',
-    term1: '01-06-2000 : 31-05-2003',
-    term2: '01-01-2014 : 31-12-2016',
-  },
-  {
-    imageUrl: richardFernandes,
-    name: 'richardfernandes',
-    term1: '01-01-2011 : 31-12-2013',
-  },
-  {
-    imageUrl: jeraldLobo,
-    name: 'jeraldlobo',
-    term1: '01-01-2017 : 31-12-2019',
-    term2: '01-01-2020 : 31-12-2020',
-  },
-  // {
-  //   imageUrl: johnsonLobo,
-  //   name: 'johnsonlobo',
-  //   period: '',
-  // },
+	{
+		imageUrl: richardFernandes,
+		name: 'richardfernandes',
+		term1: '01-01-2017 : 31-12-2019',
+		term2: '01-01-2020 : 31-12-2020',
+	},
 ];
 
-export { vpSecretaries };
+const secretaries = [
+	{
+		imageUrl: alexDsouza,
+		name: 'alexdsouza',
+		term: '01-02-1982 : 31-05-1985',
+	},
+	{
+		imageUrl: agnesRodrigues,
+		name: 'agnesrodrigues',
+		term: '01-06-1985 : 30-11-1985',
+	},
+	{
+		imageUrl: srJacintha,
+		name: 'srjacintha',
+		term: '01-12-1985 : 31-05-1986',
+	},
+	{
+		imageUrl: benedictPeris,
+		name: 'benedictperis',
+		term: '01-06-1986 : 31-05-1988',
+	},
+	{
+		imageUrl: oswaldPeris,
+		name: 'oswaldperis',
+		term1: '01-06-1988 : 31-05-1991',
+		term2: '01-06-2006 : 31-12-2010',
+	},
+	{
+		imageUrl: jeraldRasquinha,
+		name: 'jeraldrasquinha',
+		term: '01-06-1991 : 31-05-1994',
+	},
+	{
+		imageUrl: wilfredPinto,
+		name: 'wilfredpinto',
+		term1: '01-06-1994 : 31-05-2000',
+		term2: '01-06-2006 : 31-12-2010',
+	},
+	{
+		imageUrl: lillyMathias,
+		name: 'lillymathias',
+		term1: '01-06-2000 : 31-05-2003',
+		term2: '01-01-2014 : 31-12-2016',
+	},
+	{
+		imageUrl: richardFernandes,
+		name: 'richardfernandes',
+		term: '01-01-2011 : 31-12-2013',
+	},
+	{
+		imageUrl: jeraldLobo,
+		name: 'jeraldlobo',
+		term1: '01-01-2017 : 31-12-2019',
+		term2: '01-01-2020 : 31-12-2020',
+	},
+	// {
+	//   imageUrl: johnsonLobo,
+	//   name: 'johnsonlobo',
+	//   period: '',
+	// },
+]
+
+export { vicePresidents, secretaries };
+
