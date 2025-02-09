b096e9283c285c16def8dc4fdc3cdae52a8df279
Nigel <nperis27@gmail.com>
2025-02-09 09:43:12 +0530

    feat: add centenary souvenir

--- astro-frontend/src/constants/pompeichem-falkem.ts
+++ astro-frontend/src/constants/pompeichem-falkem.ts
@@ -1,6 +1,7 @@
 import pfDecemeber2024 from '~/assets/pompeichem-falkem/pf-december-2024.jpeg';
 import pfMarch2024 from '~/assets/pompeichem-falkem/pf-march-2024.jpg';
 import pfSeptember2024 from '~/assets/pompeichem-falkem/pf-september-2024.jpg';
+import centenarySouvenir from '~/assets/pompeichem-falkem/centenary-souvenir.jpg'
 
 const magazines = [
 	{
@@ -16,10 +17,16 @@ const magazines = [
 			'https://drive.google.com/uc?export=download&id=1RvnYVNiy7HOKP6jBGvGc9fJKQifDigVw',
 	},
 	{
-		title: ' March Edition',
+		title: 'March Edition',
 		imageUrl: pfMarch2024,
 		downloadUrl:
 			'https://drive.google.com/uc?export=download&id=1mHa90hb5dQbkh7RFBw_AbpznULY12U2G',
+	},
+	{
+		title: 'Centenary Edition',
+		imageUrl: centenarySouvenir,
+		downloadUrl:
+			'https://drive.google.com/uc?export=download&id=1VhxO7SjbXgdxdLgh--8eDbKMhqpLpdTC',
 	},
 ];
 
