092964b0c49097e672655539c8070c65e4313fcf
Nigel <nperis27@gmail.com>
2025-03-26 22:10:50 +0530

    feat: create strapi endpoints and add jsdocs

--- astro-frontend/src/services/events/get-event.ts
+++ astro-frontend/src/services/events/get-event.ts
@@ -1,17 +1,21 @@
 import type { Event, EventData } from '~/models/event';
-
 import { strapiFetch } from '~/helpers/strapi-fetch';
+import { EVENTS } from '~/constants/strapi-endpoints';
 
+/**
+ * Fetches a specific event from the Strapi CMS based on the provided slug.
+ *
+ * @param {string} slug - The unique slug of the event to fetch.
+ * @returns {Promise<Event | undefined>} A promise resolving to the event object if found, or `undefined` if not.
+ */
 async function getEvent(slug: string): Promise<Event | undefined> {
-	const endpoint = '/events';
-
 	const queryParams = new URLSearchParams({
 		'populate[0]': 'eventImage',
 		'filters[slug][$eq]': slug,
 	});
 
 	const data = await strapiFetch<EventData>({
-		endpoint,
+		endpoint: EVENTS,
 		queryParams,
 	});
 
--- astro-frontend/src/services/get-parish-priest-message.ts
+++ astro-frontend/src/services/get-parish-priest-message.ts
@@ -1,17 +1,21 @@
+import { PARISH_PRIEST_MESSAGE } from "~/constants/strapi-endpoints";
 import { Locale } from "~/enums/locale";
 import { strapiFetch } from "~/helpers/strapi-fetch";
 import { type ParishPriestMessage, type ParishPriestMessageData } from "~/models/parish-priest-message";
 
+/**
+ * Fetches the parish priest's message from the Strapi CMS.
+ *
+ * @param {Locale} [locale=Locale.EN] - The locale/language to fetch the message in (default: English).
+ * @returns {Promise<ParishPriestMessage | null>} A promise resolving to the parish priest's message or `null` if not found.
+ */
 async function fetchParishPriestMessage(locale: Locale = Locale.EN): Promise<ParishPriestMessage | null> {
-  const endpoint = '/parish-priest-message';
-
   const queryParams = new URLSearchParams({
     'populate[0]': 'parishPriestImage',
     'locale': locale
   });
 
-  const data = await strapiFetch<ParishPriestMessageData>({ endpoint, queryParams });
-
+  const data = await strapiFetch<ParishPriestMessageData>({ endpoint: PARISH_PRIEST_MESSAGE, queryParams });
 
   if (!data?.data) {
     return null;
