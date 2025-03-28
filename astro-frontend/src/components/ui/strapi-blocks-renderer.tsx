58309efbda08745e22bdfc306b4bf91a1f4d5471
Nigel <nperis27@gmail.com>
2025-03-28 21:30:18 +0530

    fix: use strapi blocks for popes intention

--- astro-frontend/src/components/ui/strapi-blocks-renderer.tsx
+++ astro-frontend/src/components/ui/strapi-blocks-renderer.tsx
@@ -56,7 +56,7 @@ function StrapiBlocksRenderer(props: StrapiBlocksProps) {
 
 				paragraph: ({ children }) => {
 					return (
-						<p className="font-noto-sans-kannada leading-7 text-slate-700">
+						<p className="font-noto-sans-kannada leading-7 text-slate-900">
 							{children}
 						</p>
 					);
