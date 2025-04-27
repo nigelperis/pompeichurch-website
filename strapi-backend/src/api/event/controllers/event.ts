726bc384cf513ea4519a987c83e4ba6f19701825
Aroma Peris <152790431+aromaperis@users.noreply.github.com>
2025-04-27 19:31:22 +0530

    feat: update pompei spelling in kok
    fix: remove framer motion
    
    empty commit
    
    feat: enable publish button
    
    feat: rename
    
    fix: remove framer motion
    
    empty commit

--- strapi-backend/src/api/event/controllers/event.ts
+++ strapi-backend/src/api/event/controllers/event.ts
@@ -1,27 +1,7 @@
-const { createCoreController } = require('@strapi/strapi').factories;
+/**
+ * event controller
+ */
 
-module.exports = createCoreController('api::event.event', ({ strapi }) => ({
-  async likeEvent(ctx) {
-    const { slug } = ctx.params;
+import { factories } from "@strapi/strapi";
 
-    try {
-      const event = await strapi.db.query('api::event.event').findOne({
-        where: { slug },
-      });
-
-      if (!event) {
-        return ctx.notFound('Event not found');
-      }
-
-      const updatedEvent = await strapi.entityService.update('api::event.event', event.id, {
-        data: {
-          likes: (event.likes || 0) + 1
-        }
-      });
-
-      return updatedEvent;
-    } catch (err) {
-      ctx.throw(500, err);
-    }
-  },
-}));
+export default factories.createCoreController("api::event.event");
