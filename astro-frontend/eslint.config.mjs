4b921637c0e7d8ead86a504b9e3ba5d3fd8c9dd1
Nigel <nperis27@gmail.com>
2025-05-20 11:30:11 +0530

    chore: refactor font usage across components and pages to streamline styling

--- astro-frontend/eslint.config.mjs
+++ astro-frontend/eslint.config.mjs
@@ -1,5 +1,5 @@
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginAstro from 'eslint-plugin-astro';
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
 
 /** @type {import('@typescript-eslint/utils').TSESLint.FlatConfig.ConfigFile} */
 export default [...eslintPluginAstro.configs.recommended, eslintConfigPrettier];
