4b071606ad920226be91dd2730ae3ae34038c08d
Aroma Peris <152790431+aromaperis@users.noreply.github.com>
2025-05-23 23:57:43 +0530

    chore/fixes (#189)
    * fix: obituary metadata and commissions ui
    
    * refactor: obitaury meta data
    
    * chore: updgrade node version
    
    * chore: remove unused imports

---
+++ astro-frontend/src/enums/ward-names-kok.ts
@@ -0,0 +1,12 @@
+export enum WardNamesKok {
+  "Kowdoor A" = "ಕೌಡೂರ್ ಎ",
+  "Kowdoor B" = "ಕೌಡೂರ್ ಬಿ",
+  "Pompei A" = "ಪೊಂಪೈ ಎ",
+  "Pompei B" = "ಪೊಂಪೈ ಬಿ",
+  "Kandar A" = "ಕಂದಾರ್ ಎ",
+  "Kandar B" = "ಕಂದಾರ್ ಬಿ",
+  "Monel" = "ಮಣೆಲ್",
+  "Gurpur" = "ಗುರ್ಪುರ್",
+  "Church" = "ಇಗರ್ಜೆ",
+  "Addoor" = "ಅಡ್ಡೂರ್",
+}
