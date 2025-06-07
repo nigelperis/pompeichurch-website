fdf44b9d5a4449aeab916a2802607f1f6ec00756
Avinash Valder <avinashvalder7@gmail.com>
2025-06-07 18:21:19 +0530

    fix: hoverable link styles

--- astro-frontend/src/components/ObituaryCardMin.tsx
+++ astro-frontend/src/components/ObituaryCardMin.tsx
@@ -29,7 +29,7 @@ interface Props {
 // Simple lang detection from URL for label localization
 const lang =
   typeof window !== "undefined" &&
-    window.location.pathname.startsWith(`/${Locale.KOK}`)
+  window.location.pathname.startsWith(`/${Locale.KOK}`)
     ? Locale.KOK
     : Locale.EN;
 
@@ -226,7 +226,7 @@ export default function ObituaryCardMin({
         <div className="absolute inset-0 z-5 flex items-center justify-center lg:hidden">
           <a
             href={lang === Locale.KOK ? "/kok/obituary" : "/obituary"}
-            className="from-natgeo-yellow to-natgeo-yellow hoverable-link border-natgeo-yellow border-b-2 bg-gradient-to-r px-2 py-1 inline-block text-xl font-bold text-black hover:border-transparent hover:text-black"
+            className="from-natgeo-yellow to-natgeo-yellow hoverable-link border-natgeo-yellow border-b-2 bg-gradient-to-r px-2 py-1 inline-block text-xl font-bold text-black hover:border-transparent hover:text-black mb-2 mt-2"
           >
             {t ? t("ui.view-all") : "View All"}
           </a>
