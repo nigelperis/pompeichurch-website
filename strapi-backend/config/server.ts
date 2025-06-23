bb965d60ab632089291806440ca4eaa113bf1a0c
Avinash Valder <68838387+avinashvalder@users.noreply.github.com>
2025-06-23 19:56:51 +0530

    Revert "feat: update middleware and server configurations to support larger f…"
    This reverts commit 8d9355630b90d9722f35419e97b5dc43aa1b4c73.

--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -1,18 +1,13 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
+import cronTasks from "./cron-tasks";
+
+export default ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
   app: {
-    keys: env.array("APP_KEYS"),
+    keys: env.array('APP_KEYS'),
   },
-  webhooks: {
-    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
-  },
-  // Add body parser configuration
-  parser: {
+  cron: {
     enabled: true,
-    multipart: true,
-    formidable: {
-      maxFileSize: 300 * 1024 * 1024, // 300MB
-    },
-  },
+    tasks: cronTasks,
+  }
 });
