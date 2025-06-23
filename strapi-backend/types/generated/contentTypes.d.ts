6c6fef99897285f26361e191a35a91b39009f2a3
SANTHSIM22 <santhsimviriledsouza123456@gmail.com>
2025-06-23 23:51:39 +0530

    Feat: add events backend changes

--- strapi-backend/types/generated/contentTypes.d.ts
+++ strapi-backend/types/generated/contentTypes.d.ts
@@ -777,6 +777,21 @@ export interface ApiEventEvent extends Struct.CollectionTypeSchema {
     draftAndPublish: true;
   };
   attributes: {
+    associationName: Schema.Attribute.Enumeration<
+      [
+        'SVP',
+        'ICYM-PYC',
+        'Altar Servers',
+        'Catholic Sabha',
+        'YCS',
+        'Secular Franciscan',
+        'Catechism',
+        'Small Christian Community',
+        'Gurpur Church Choir',
+        'Women\u2019s Forum',
+        'Christian\u00A0Life\u00A0Community',
+      ]
+    >;
     createdAt: Schema.Attribute.DateTime;
     createdBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
