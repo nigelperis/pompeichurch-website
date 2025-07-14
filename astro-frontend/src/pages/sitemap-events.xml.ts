510f66625ad461b2d151930f2d7dfc5ac53d54a7
Aroma Peris <paroma3008@gmail.com>
2025-07-15 00:19:04 +0530

    fix: sitemap rendering for more than 25 entries

--- astro-frontend/src/pages/sitemap-events.xml.ts
+++ astro-frontend/src/pages/sitemap-events.xml.ts
@@ -1,16 +1,26 @@
 import type { APIRoute } from "astro";
-import type { Event } from "../models/event";
-import { Locale } from "../enums/locale";
-import { SITE_URL } from "../constants/constants";
+import type { Event } from "~/models/event";
+import { Locale } from "~/enums/locale";
+import { SITE_URL } from "~/constants/constants";
+import { listEvents } from "~/services/events/list-events";
 
 export const GET: APIRoute = async () => {
   const baseURL = import.meta.env.PUBLIC_STRAPI_URL;
 
   const entry: string[] = [];
-  const events = await fetch(`${baseURL}/api/events`);
-  const eventsData = await events.json();
+  const allEvents: Event[] = [];
+  
+  let currentPage = 1;
+  let totalPages;
 
-  eventsData.data.forEach((event: Event) => {
+  do {
+    const events = await listEvents({ page: currentPage });
+    allEvents.push(...events.events);
+    currentPage++;
+    totalPages = events.pagination.pageCount;
+  } while (currentPage <= totalPages);
+
+  allEvents.forEach((event: Event) => {
     entry.push(
       `<url>
             <loc>${SITE_URL}/events/${event.slug}</loc>
@@ -32,7 +42,7 @@ export const GET: APIRoute = async () => {
     );
   });
 
-  const xml = `<?xml version="1.0" encoding="UTF-8"?> 
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
             ${entry.join("\n")}
     </urlset>`;
