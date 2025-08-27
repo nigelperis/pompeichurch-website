85f7685ecba97345af441a5556fea137513a0ed8
Nigel Peris <nperis27@gmail.com>
2025-08-27 19:57:52 +0530

    chore: remove form and criteria

--- astro-frontend/src/constants/index.ts
+++ astro-frontend/src/constants/index.ts
@@ -5,16 +5,6 @@ export const RECIPIENT_EMAILS = [
 ];
 export const SITE_URL = "https://pompeichurch.in";
 
-export const SEND_ACHIEVEMENT_API_URL = "/api/send-achievement";
-
 export const ACHIEVEMENTS_PER_PAGE = 6;
 export const EVENTS_PER_PAGE = 6;
 export const OBITUARIES_PER_PAGE = 6;
-
-export const MAX_SIZE = 2 * 1024 * 1024;
-export const MAX_IMAGES = 2;
-
-export const STD_LIMIT = 50;
-export const DESCRIPTIVE_LIMIT = 100;
-
-export const TRUNCATE_AT = 20;
