54e540730390205c1a8f9e04dd032a056a4b4ff6
Nigel Peris <nperis27@gmail.com>
2025-08-27 19:59:17 +0530

    fix: add recipients back

--- astro-frontend/src/constants/index.ts
+++ astro-frontend/src/constants/index.ts
@@ -1,7 +1,9 @@
 export const ADMIN_EMAIL = "notification@pompeichurch.in";
 export const RECIPIENT_EMAILS = [
-
   "paroma3008@gmail.com",
+  "nperis27@gmail.com",
+  "avinashvaldar7@gmail.com",
+  "ourladyofpompeichurch@gmail.com"
 ];
 export const SITE_URL = "https://pompeichurch.in";
 
