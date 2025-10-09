cb2e2eaab391975438fb6eda63f0033ce0da54ac
Nigel Peris <nperis27@gmail.com>
2025-10-09 11:19:30 +0530

    hotfix: proxy

--- strapi-backend/config/server.ts
+++ strapi-backend/config/server.ts
@@ -3,7 +3,9 @@ import cronTasks from "./cron-tasks";
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  proxy: true,
+  proxy: {
+    koa: true,
+  },
   app: {
     keys: env.array('APP_KEYS'),
   },
