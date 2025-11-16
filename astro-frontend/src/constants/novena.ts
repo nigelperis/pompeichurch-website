369f7e82a7e62311dbc25bd2a53a7de8c6892256
Ashton Dsilva <21i06.ashton@sjec.ac.in>
2025-11-16 21:59:58 +0530

    chore: added the english version of the novena

--- astro-frontend/src/constants/novena.ts
+++ astro-frontend/src/constants/novena.ts
@@ -1,13 +1,13 @@
-const novenaPrayer = `ಏ ಪಾಪಾ ಖತಾವಿಣೆ ಜಲ್ಮಲ್ಲ್ಯೆ ಆಂಕ್ವಾರಿ, ಆನಿ ಪವಿತ್ರ್‌ ರೊಜಾರಿಚ್ಯೆ ರಾಣಿಯೆ, ಭಾವಾಡ್ತ್‌ ಅಸ್ಕತ್‌ ಜಾವ್ನ್‌, ಖೊಟೆಪಣ್‌ ಜಯ್ತೆವಂತ್‌ ಜಾಂವ್ಚಾ ಹ್ಯಾ ದಿಸಾಂನಿ, ಆದಿಂಮಾಗಾಂ ಅನ್ಫಾವಾಡ್ತಿ ಆಸ್‌ಲ್ಲ್ಯಾ ಪೊಂಪೈಚ್ಯಾ ಪುರ್ವಿಲ್ಯಾ ಗಾಂವಾಂತ್‌, ತುಂ ರಾಣಿ ಆನಿ ಆವಯ್‌ ಜಾವ್ನ್‌ ತುಜೊ ಪಾಟ್‌ ಬಾಂಧುಂಕ್‌ ತುಂವೆಂ ಖುಶಿ ವ್ಹೆಲ್ಯಾಯ್‌, ಬೊಂಬ್ಯಾಂಕ್‌ ಆನಿ ದೆಂವ್ಚಾರಾಂಕ್‌ ನಮಸ್ಕಾರ್‌ ಕರ್ನ್‌ ಆಯಿಲ್ಲ್ಯಾ ಜಾಗ್ಯಾ ಥಾವ್ನ್‌, ತುಂ ಆಜ್‌, ದೇವ್‌ - ಕುರ್ಪೆಚ್ಯೆ ಆವಯ್‌ಬರಿ ಸರ್ಗಿಂಚಿಂ ಕಾಕ್ಳುತಿಚಿಂ ಭಂಡಾರಾಂ ಸಂಸಾರಾರ್‌ ಚಾರಾಂಯಿ ಕುಶಿಂನಿ ಉದಾರ್‌ ಮನಾನ್‌ ವಾಂಟ್ತಾಯ್‌. ತುಂವೆಂ ಎದ್ಯೆ ವ್ಹಡ್ಲೆ ಕಾಕ್ಳುತಿನ್ ರಾಜ್ಜಟ್‌ ಚಲಂವ್ಚಾ ತ್ಯಾ ಪಾಟಾ ಥಾವ್ನ್‌ ಮೊಜೆರ್‌ಯಿ, ಏ ಆಂಕ್ಚಾರಿ ಮರಿಯೆ, ತುಜೆ ಕಾಕ್ಳುತಿಚೆ ದೊಳೆ ಪರ್ತಿ, ಆನಿ ತುಜಾ ಆಧಾರಾಚಾ ಎದ್ಯಾ ವ್ಹಡಾ ಗರ್ಜೆಂತ್‌ ಆಸ್ಚಾ ಮೊಜಿ ಕಾಕ್ಳುತ್‌ ಕರ್‌, ಕಿತ್ಲೆಶ್ಯಾ ಜಣಾಂಕ್‌ ತುಂ ಕಾಕ್ಳುತಿಚಿ ಮಾಯ್‌ ಜಾವ್ನ್‌ ಆಸಾಯ್‌ ಮ್ಹಣ್‌ ತುಂವೆಂ ದಾಕಯಿಲ್ಲೆಬರಿ ಮ್ಹಾಕಾಯಿ ದಾಕಯ್‌. ಆನಿ ಹಾಂವ್‌ ಸಗ್ಳ್ಯಾ ಮೊಜಾ ಕಾಳ್ಜಾನ್‌ ತುಕಾ, ಮೊಜಿ ಸಾಯ್ದಿಣ್‌ ಆನಿ ಪವಿತ್ರ್‌ ರೊಜಾರಿಚಿ ರಾಣಿ ಮ್ಹಣ್‌ ಪಾಚಾರ್ತಾಂ ಆನಿ ಉಲೊ ಮಾರ್ತಾಂ.
+const novenaPrayer = `O Immaculate Virgin and Queen of the Holy Rosary, in these times of dead faith and triumphant impiety you have desired to establish your throne of Queen and Mother in the ancient land of Pompeii, the resting place of deceased pagans, from this place in which idols and demons were once worshipped you now as the Mother of divine grace shower the treasures of heavenly mercy far and wide, O Mary from this throne upon which you graciously reign turn upon me as well your benign eyes and have mercy on me who am so greatly in need of your help, show yourself to me just as you have shown yourself to so many others as the true Mother of mercy, with all my heart I greet you and invoke you as my Sovereign and Queen of the Holy Rosary.
 <br><br>
 
-<span style="color: red;">ನಮಾನ್‌ ರಾಣಿಯೆ, ಕಾಕ್ಳುತಿಚ್ಯೆ ಮಾಯೆ..... (ತುಕಾ ಜಾಯ್‌ ಜಾಲ್ಲೆ ಉಪ್ಕಾರ್‌ ಮಾಗ್‌)</span>  <br><br>
+<span style="color: red;">Hail Holy Queen, Mother of Mercy..... (Pray for your intentions‌)</span>  <br><br>
 
-<b>ಉಗ್ಡಾಸ್‌ ಕರ್‌, ಏ ಭೋವ್‌ ದಯಾಳಿ ಆಂಕ್ಟಾರ್‌ ಮರಿಯೆ,</b> ತುಜಾ ಆಸ್ರಾಕ್‌ ಧಾಂವ್‌ಲ್ಲೊ, ತುಜೊ ಆಧಾರ್‌ ಮಾಗ್‌ಲ್ಲೊ, ತುಜಿ ಮಧ್ಯಸ್ತಿ ಸೊಧ್‌ಲ್ಲೊ ಕೊಣ್‌ಚ್‌ ತುಜಿ ಕುಮಕ್‌ ಮೆಳಾನಾಸ್ತಾನಾ ಪಾಟಿಂ ಗೆಲೊ ಮ್ಹಣ್‌ ಕೆದಿಂಚ್, ಕೊಣೆಂಯಿ ಆಯ್ಕೊಂಕ್‌ ನಾ. ಹ್ಯೆ ಪಾತ್ಯೆಣೆನ್‌ ಧಯ್ರ್‌ ಕಾಣ್ಟೆವ್ನ್‌ ತುಜ್ಕೆ ಸರ್ಶಿಂ ಧಾಂವೊನ್‌ ಯೆತಾಂ. ಏ ಆಂಕ್ವಾರಿಂಚ್ಯೆ ಆಂಕ್ವಾರಿ, ಮೊಜ್ಯೆ ಮಾಯೆ, ತುಜ್ಯೆ ಸರ್ಶಿಂ ಯೆತಾಂ; ಪಾತ್ಕಿ ಆನಿ ಖಂತಿಷ್ಟ್‌ ತುಜ್ಯೆ ಮುಖಾರ್‌ ರಾವ್ತಾಂ; ಮನಿಸ್‌ ಜಾಲ್ಲ್ಯಾ ದೇವ್‌-ಪುತಾಚ್ಯೆ ಆನಿ ಮೊಜೆಯಿ ಮಾಯೆ, ಮೊಜಿಂ ಮಾಗ್ಣಿಂ ತಿರಸ್ಕಾರ್‌ ಕರಿನಾಕಾ. ಪುಣ್‌ ತುಜ್ಯೆ ಕಾಕ್ಳುತಿನ್‌ ಮೊಜೆಂ ಮಾಗ್ಣೆಂ  ಆಯ್ಕ್‌ ಆನಿ ಹಾಂವ್‌ ಮಾಗ್ತಾಂ ತೆಂ ಮ್ಹಾಕಾ ದಿ. ಆಮೆನ್‌.
+<b>Remember, O most gracious Virgin Mary,</b> that never was it known that anyone who fled to your protection, implored your help, or sought your intercession, was left unaided; inspired by this confidence I fly unto you, O Virgin of virgins my Mother, to you do I come, before you I stand sinful and sorrowful; O Mother of the Word Incarnate, despise not my petitions but in your mercy hear and answer me, Amen.
 <br><br>
 
-<span style="color: red;">ಯಾ : ಬೋವ್‌ ಪವಿತ್ರ್‌ ರೊಜಾರಿಚ್ಯೆ ರಾಣಿಯೆ, ಆಮಾ ಪಾಸುನ್‌ ವಿನತಿ ಕರ್‌.</span><br>
-<b>ಲೊ: ಕ್ರಿಸ್ತಾಚಾ ಭಾಸಾವ್ಣ್ಯಾಂಕ್‌ ಆಮಿ ಫಾವೊ ಜಾಂವ್ಚೆ ಖಾತಿರ್‌.</b><br><br>
+<span style="color: red;">L : Most Holy Queen of the Rosary, intercede for us.</span><br>
+<b>R: That we may be made worthy of the promises of Christ.‌.</b><br><br>
 
-ಪ್ರಾರ್ಥುಯಾಂ : ಏ ದೆವಾ, ತುಜಾ ಎಕ್ಲ್ಯಾಚ್ ಪುತಾನ್‌ ಆಪ್ಲ್ಯಾ ಜಿಣಿಯೆ, ಮೊರ್ನಾ ಆನಿ ಜಿವಂತ್ಪಣಾ ವರ್ವಿಂ ಸಾಸ್ಣಾಚ್ಯೆ ಬಚಾವೆಚೆಂ ಇನಾಮ್‌ ಆಮ್ಕಾಂ ಜೊಡ್ನ್‌ ದವರ್ಲಾಂ; ಭಾಗೆವಂತ್‌ ಆಂಕ್ಟಾರ್‌ ಮರಿಯೆಚಾ ಬೋವ್‌ ಪವಿತ್ರ್‌ ರೊಜಾರಿಚೆ ಮಿಸ್ತೆರ್‌ ಆಮಿ ನಿಯಾಳ್ತಾನಾ, ತಾಂತುಂ ಭಾಸಾಯಿಲ್ಲೆಂ ಆಮಿ ಜೊಡಿಶೆಂ ಕರ್‌ ಮ್ಹಣ್‌ ಆಮಿ ತುಜೆಲಾಗಿಂ ಪರಾತ್ತಾಂವ್‌. ತ್ಯಾಚ್‌ ಕ್ರಿಸ್ತಾ ಆಮ್ಚಾ ಸೊಮಿಯಾ ಮಾರಿಫಾತ್‌. ಆಮೆನ್‌.`;
+Let us pray : O God, whose Only Begotten Son, by His life, death, and resurrection, has obtained for us the reward of eternal salvation; grant, we pray, that as we meditate upon the Most Holy Mysteries of the Blessed Virgin Mary’s Rosary, we may imitate what they contain and obtain what they promise. Through the same Christ our Lord. Amen.`;
 export { novenaPrayer };
