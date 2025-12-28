0f8b0292aef7ee4c250cf7733f2b2336c215d275
Ashton Dsilva <21i06.ashton@sjec.ac.in>
2025-12-28 12:06:00 +0530

    Formatting

--- astro-frontend/src/helpers/is-christmas.ts
+++ astro-frontend/src/helpers/is-christmas.ts
@@ -1,13 +1,13 @@
 export function isChristmasSeason(): boolean {
   const now = new Date(
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
   );
   return now.getMonth() === 11;
 }
 
 export function isChristmasWeek(): boolean {
   const now = new Date(
-    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" })
+    new Date().toLocaleString("en-US", { timeZone: "Asia/Kolkata" }),
   );
 
   const month = now.getMonth();
