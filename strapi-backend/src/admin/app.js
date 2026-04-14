5c03eb7357b27bad6820a985b6493e8538413ea8
ashleendsilva <ashleendsilva@gmail.com>
2026-04-14 21:58:49 +0530

    feat: rename logo.png to church-logo.png

---
+++ strapi-backend/src/admin/app.js
@@ -0,0 +1,14 @@
+import AuthLogo from "../extensions/church-logo.png";
+import MenuLogo from "../extensions/church-logo.png";
+
+export default {
+  config: {
+    auth: {
+      logo: AuthLogo,
+    },
+    menu: {
+      logo: MenuLogo,
+    },
+  },
+  bootstrap() {},
+};
